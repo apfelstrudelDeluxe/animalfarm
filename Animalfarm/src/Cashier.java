d5eaeb6a5b1771bf933d725d84f5eca162aa88e1
karinkraken <ka.thuerschmid@gmail.com>
2020-10-16 17:00:29 +0200

    cashier+sale mit super

--- Animalfarm/src/Cashier.java
+++ Animalfarm/src/Cashier.java
@@ -1,14 +1,18 @@
 import java.util.UUID;
 
-public class Cashier {
+public class Cashier extends Employee{
 
     private String keyId;
     private String cashierStation;
     private double revenue;
 
-    public Cashier(String keyId, String cashierStation, double revenue) {
+    public Cashier(String firstName, String secondName, int svn, Location location, String sex, int salary, Address address, String keyId, String cashierStation, double revenue) {
+        super(firstName, secondName, svn, location, sex, salary, address);
         this.keyId = keyId;
-        this.cashierStation = UUID.randomUUID().toString();;
+        this.cashierStation = UUID.randomUUID().toString();
         this.revenue = revenue;
     }
 }
+
+
+
