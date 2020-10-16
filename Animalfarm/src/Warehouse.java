ccfe89383beccaca1bf7b1dd3b9473a0f8767db2
wolfsgirl13 <wolfsgirl13@hotmail.com>
2020-10-16 16:56:25 +0200

    first

---
+++ Animalfarm/src/Warehouse.java
@@ -0,0 +1,6 @@
+public class Warehouse extends Employee  {
+
+    public Warehouse (String firstName, String secondName, int svn, Location location, String sex, int salary, Address address) {
+        super(firstName, secondName, svn, location, sex, salary, address);
+    }
+}
