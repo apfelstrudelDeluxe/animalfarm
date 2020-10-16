879321a64dcad0f46fc81a63140d0dac3ce93b29
U787020 <66324795+U787020@users.noreply.github.com>
2020-10-16 17:41:57 +0200

    Added Office

--- Animalfarm/src/Office.java
+++ Animalfarm/src/Office.java
@@ -4,10 +4,11 @@ private CoffeeCard coffeeCard;
 private KeyCard keyCard;
 private int parkingSpot;
 
-   public Office(String firstName, String secondName, int svn, Location location, String sex, int salary, Address address, CoffeeCard coffeeCard, KeyCard keyCard) {
+   public Office(String firstName, String secondName, int svn, Location location, String sex, int salary, Address address, CoffeeCard coffeeCard, KeyCard keyCard, int parkingSpot) {
       super(firstName, secondName, svn, location, sex, salary, address);
       this.coffeeCard = coffeeCard;
       this.keyCard = keyCard;
+      this.parkingSpot = parkingSpot;
    }
 }
 
