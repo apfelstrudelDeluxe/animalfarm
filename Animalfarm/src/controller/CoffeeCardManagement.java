b556b810dfbd5178fba9e0307f27f2d4f694e94e
ToniM1810 <antonija.mlinar@gmx.at>
2020-10-23 17:16:55 +0200

    CoffeeCardList-Location

--- Animalfarm/src/controller/CoffeeCardManagement.java
+++ Animalfarm/src/controller/CoffeeCardManagement.java
@@ -19,9 +19,9 @@ public class CoffeeCardManagement {
         this.current = LocalDateTime.now();
 
         this.coffeeCards = new ArrayList<>();
-        coffeeCards.add (new CoffeeCard(new Employee("Franz", "Huber", 758945, new Location(), "male", 1500, new Address(" ")), 2021-01-01, 20.80));
-        coffeeCards.add( new CoffeeCard(new Employee("Helga", "Mair", 123456, new Location(), "male", 1500, new Address(" ")), 2020-01-01, 0.20));
-        coffeeCards.add( new CoffeeCard(new Employee("Daniel", "Hofer", 654321, new Location(), "male", 1500, new Address(" ")), 2022-04-05, 4.50));
+        coffeeCards.add (new CoffeeCard(new Employee("Franz", "Huber", 758945, new Location("Hauptplatz", "Linz", 4020, "Austria"), "male", 1500, new Address(" ")), 2021-01-01, 20.80));
+        coffeeCards.add( new CoffeeCard(new Employee("Helga", "Mair", 123456, new Location("Hauptplatz", "Linz", 4020, "Austria"), "male", 1500, new Address(" ")), 2020-01-01, 0.20));
+        coffeeCards.add( new CoffeeCard(new Employee("Daniel", "Hofer", 654321, new Location("Hauptplatz", "Linz", 4020, "Austria"), "male", 1500, new Address(" ")), 2022-04-05, 4.50));
 
     }
 
