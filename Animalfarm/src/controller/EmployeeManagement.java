5b5c079c06583e3f4f349313db327d6e1554a785
Lukas Wais <lukas.wais@outlook.de>
2020-11-06 15:31:15 +0100

    change wrong super constructor in Warehouse.java

--- Animalfarm/src/controller/EmployeeManagement.java
+++ Animalfarm/src/controller/EmployeeManagement.java
@@ -10,38 +10,29 @@ public class EmployeeManagement {
     private final List<Employee> employees;
 
     public EmployeeManagement() {
-
         this.employees = new ArrayList<>();
-
     }
 
     public Employee getEmp(int svn) {
-
         for (Employee e : employees) {
             if (e.getSvn() == svn) {
                 return e;
             }
         }
-
         return null;
-
     }
 
     public double revSum() {
-
         double rev = 0;
         for (Employee e : employees) {
             if (e instanceof Cashier) {
                 rev += ((Cashier) e).getRevenue();
             }
         }
-
         return rev;
-
     }
 
     public boolean deleteEmp(int svn) {
-
         Employee e = getEmp(svn);
         if (e == null) {
             return false;
@@ -50,42 +41,26 @@ public class EmployeeManagement {
     }
 
     public boolean addEmp(Employee e) {
-
         return employees.add(e);
-
     }
 
-
     public List<String> getLic(Employee e) {
-
         if (e instanceof Warehouse) {
-
             return ((Warehouse) e).getLicenses();
-
         }
-
         return null;
     }
 
-
     public double getRev(Employee e) {
-
         if (e instanceof Cashier) {
-
             return ((Cashier) e).getRevenue();
-
         }
-
         return -1;
     }
 
     public double getBoni(Employee e) {
-
         if (e instanceof Warehouse) {
-
             return ((Warehouse) e).getBonus();
-
-
         }
         return -1;
     }
--- Animalfarm/src/model/Warehouse.java
+++ Animalfarm/src/model/Warehouse.java
@@ -7,7 +7,7 @@ public class Warehouse extends Employee  {
     private final List<String> licenses;
 
     public Warehouse (String firstName, String secondName, int svn, Location location, String sex, int salary, Address address, double bonus, List<String> licenses) {
-        super(firstName, secondName, svn, location, sex, salary, address);
+        super(firstName, secondName, svn, address, sex, salary, location);
         this.bonus = bonus;
         this.licenses = licenses;
     }
