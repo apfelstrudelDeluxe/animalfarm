8ba4d81ca092770f144ebb741a5465429117fd00
karinkraken <ka.thuerschmid@gmail.com>
2020-11-27 16:58:18 +0100

    Coffee Card Menu

--- Animalfarm/src/model/CoffeeCard.java
+++ Animalfarm/src/model/CoffeeCard.java
@@ -19,3 +19,26 @@ public class CoffeeCard extends Card {
         this.credit += credit;
     }
 }
+
+
+
+
+        ​
+public class CoffeeCard extends Card {
+    private double credit;
+    public CoffeeCard(LocalDate dueDate, double credit) {
+        super(dueDate, "c" + UUID.randomUUID());
+        this.credit = credit;
+​
+    }
+​
+    public void setCredit(double credit) {
+        this.credit = credit;
+​
+​
+    }
+​
+    public double getCredit() {
+        return credit;
+    }
+}
